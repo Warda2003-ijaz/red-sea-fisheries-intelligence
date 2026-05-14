696cc31803da19ce074846aa01537bbe9d624b0f
Warda2003-ijaz <wardaijaz2003@gmail.com>
2026-05-13 21:56:19 -0700

    Update app.py

--- app.py
+++ app.py
@@ -18,9 +18,13 @@ st.markdown("---")
 # Load Data (Cached to make the app fast)
 @st.cache_data
 def load_and_clean_data():
-    capture_df = pd.read_csv('capture_quantity.csv')
-    country_df = pd.read_csv('cl_fi_country_groups.csv')
-    species_df = pd.read_csv('cl_fi_species_groups.csv')
+    #capture_df = pd.read_csv('capture_quantity.csv')
+    #country_df = pd.read_csv('cl_fi_country_groups.csv')
+    #species_df = pd.read_csv('cl_fi_species_groups.csv')
+    # Fully resilient data parsers configured to bypass encoding blocks and skip corrupt markers
+    capture_df = pd.read_csv('capture_quantity.csv', encoding='utf-8', encoding_errors='ignore')
+    country_df = pd.read_csv('cl_fi_country_groups.csv', encoding='utf-8', encoding_errors='ignore')
+    species_df = pd.read_csv('cl_fi_species_groups.csv', encoding='utf-8', encoding_errors='ignore')
 
     country_clean = country_df[
         ['UN_Code', 'ISO3_Code', 'Name_En', 'Continent_Group_En', 'EcoClass_Group_En', 'GeoRegion_Group_En']]
